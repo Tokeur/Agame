8fdd1d12af8ab8eb70f48d45b90e6539b589b0ff
Tokeur <tokeur29@gmail.com>
2014-03-03 18:27:20 +0100

    main.cpp
    Added some stuff in main.cpp

--- Agame/main.cpp
+++ Agame/main.cpp
@@ -1,7 +1,36 @@
 #include <iostream>
 #include <SFML/Graphics.hpp>
 
+using namespace std;
+
 int main()
 {
+    sf::RenderWindow window(sf::VideoMode(640,480), "AGame");
+
+    while(window.isOpen())
+    {
+        sf::Event ev;
+        while(window.pollEvent(ev))
+        {
+            switch (ev.type)
+            {
+            case sf::Event::KeyPressed:
+                switch (ev.key.code)
+                {
+                case sf::Keyboard::Escape:
+                    window.close();
+                    break;
+                }
+                break;
+             case sf::Event::Closed:
+                window.close();
+                break;
+            }
+        }
+
+        window.clear();
+        window.display();
+    }
+
     return EXIT_SUCCESS;
 }
