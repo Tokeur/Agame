0bfdc088f3e3fea172f3170f71ab8646da8081ed
DeinDall <dev.deindall@gmail.com>
2014-04-30 12:56:05 +0200

    TAB TAB TAB TAB EVERYWHERE

--- Agame/main.cpp
+++ Agame/main.cpp
@@ -3,34 +3,31 @@
 
 using namespace std;
 
-int main(int, char**)
-{
-    sf::RenderWindow window(sf::VideoMode(640,480), "AGame");
+int main(int, char**) {
+	sf::RenderWindow window(sf::VideoMode(640,480), "AGame");
 
-    while(window.isOpen())
-    {
-        sf::Event ev;
-        while(window.pollEvent(ev))
-        {
-            switch (ev.type)
-            {
-            case sf::Event::KeyPressed:
-                switch (ev.key.code)
-                {
-                case sf::Keyboard::Escape:
-                    window.close();
-                    break;
-                }
-                break;
-             case sf::Event::Closed:
-                window.close();
-                break;
-            }
-        }
+	while(window.isOpen()) {
+		sf::Event ev;
 
-        window.clear();
-        window.display();
-    }
+		while(window.pollEvent(ev)) {
+			switch (ev.type) {
+			case sf::Event::KeyPressed:
+				switch (ev.key.code) {
+				case sf::Keyboard::Escape:
+					window.close();
+				break;
+				}
+			break;
 
-    return EXIT_SUCCESS;
+			case sf::Event::Closed:
+				window.close();
+			break;
+			}
+		}
+
+		window.clear();
+		window.display();
+	}
+
+	return EXIT_SUCCESS;
 }
